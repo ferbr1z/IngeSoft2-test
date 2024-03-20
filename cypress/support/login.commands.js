1e32912fe5165d6afb62e3ee25ebb4f6a8cdbfb8
Fernando Brizuela <ffba97@gmail.com>
2024-03-20 17:02:34 -0300

    Añadido nuevos comandos y refactorización

--- cypress/support/login.commands.js
+++ cypress/support/login.commands.js
@@ -1,20 +1,31 @@
 import { API_LOGIN } from "../api-dirs";
 import { APP_URL } from "../front-dirs";
 
-Cypress.Commands.add("login", (email, password) => {
-    // Logeamos
-    cy.visit(APP_URL);
-    cy.get("#login-email").type(email);
-    cy.get("#login-password").type(password);
-    cy.get("#login").click();
-})
+Cypress.Commands.add("completarFormularioLogin", (email, password) => {
+  cy.get("#login-email").type(email);
+  cy.get("#login-password").type(password);
+});
 
-Cypress.Commands.add("loginSuccess", (email, password) => {
+Cypress.Commands.add("login", (email, password) => {
   // Vamos a interceptar la llamada a la api
   cy.intercept("POST", API_LOGIN).as("loginRequest");
 
-  cy.login(email, password);
+  cy.visit(APP_URL);
+  cy.completarFormularioLogin(email, password);
+
+  cy.get("#login").click();
 
   // Espera que la api responda
   cy.wait("@loginRequest").its("response.statusCode").should("eq", 200);
 });
+
+Cypress.Commands.add("loginFail", (email, password) => {
+  // Logeamos
+  cy.visit(APP_URL);
+  
+  cy.completarFormularioLogin(email, password);
+  cy.get("#login").click();
+
+  cy.get(".go4109123758").should("exist");
+
+})
--- cypress/support/producto.commands.js
+++ cypress/support/producto.commands.js
@@ -1,56 +1,75 @@
 import { API_PRODUCTOS } from "../api-dirs";
 
 Cypress.Commands.add("completarFormularioProducto", (producto) => {
-    cy.get("#nombre").type(producto.nombre);
-    cy.get("#descripcion").type(producto.descripcion);
-    cy.get("#codigo").type(producto.codigo);
-    cy.get("#costo").type(producto.costo);
-    cy.get("#precio").type(producto.precio);
-    cy.get("#cantidad").type(producto.cantidad);
-    cy.get("#iva").select(producto.iva);
+    cy.get("#nombre").clear();
+    producto.nombre && cy.get("#nombre").type(producto.nombre);
+    cy.get("#descripcion").clear();
+    producto.descripcion && cy.get("#descripcion").type(producto.descripcion);
+    cy.get("#codigo").clear();
+    producto.codigo && cy.get("#codigo").type(producto.codigo);
+    cy.get("#costo").clear();
+    producto.costo && cy.get("#costo").type(producto.costo);
+    cy.get("#precio").clear();
+    producto.precio && cy.get("#precio").type(producto.precio);
+    cy.get("#cantidad").clear();
+    producto.cantidad && cy.get("#cantidad").type(producto.cantidad);
+    producto.iva && cy.get("#iva").select(producto.iva);
     cy.get("#btn-guardar").click();
-  })
-  
-  Cypress.Commands.add("crearProducto", (producto) => {
+});
+
+Cypress.Commands.add("crearProducto", (producto) => {
     cy.intercept("POST", API_PRODUCTOS).as("crearProducto");
-  
+
     cy.get("#btn-crear").click();
     cy.completarFormularioProducto(producto);
-  
-    cy.crearProducto(producto);
+
     cy.wait("@crearProducto").its("response.statusCode").should("eq", 201);
-  
+
     cy.get("@crearProducto").should((req) => {
-      expect(req.request.body.nombre).to.deep.equal(producto.nombre);
+        expect(req.request.body.nombre).to.deep.equal(producto.nombre);
     });
-  });
-  
-  
-  Cypress.Commands.add("crearProductoFail", (producto) => {
+});
+
+Cypress.Commands.add("crearProductoFail", (producto) => {
     cy.get("#btn-crear").click();
     cy.completarFormularioProducto(producto);
     // Comprueba de que se muestre el mensaje de error
     cy.get(".go3958317564").should("exist");
     cy.get(".go685806154").should("exist");
-  });
-  
-  Cypress.Commands.add("buscarProducto", (producto) => {
+});
+
+Cypress.Commands.add("buscarProducto", (producto) => {
     cy.get("#input-search").clear();
     cy.get("#input-search").type(producto.nombre);
     cy.get("#btn-buscar").click();
     cy.get("table tr:first-child td:first-child")
-      .first()
-      .contains("td", producto.nombre);
-  });
-  
-  Cypress.Commands.add("borrarProducto", (producto) => {
+        .first()
+        .contains("td", producto.nombre);
+});
+
+Cypress.Commands.add("editarProducto", (producto, productoUpdate) => {
     cy.buscarProducto(producto);
-  
+    cy.get('a[id^="btn-editar-producto-"]:first').click();
+    cy.completarFormularioProducto(productoUpdate);
+    cy.get(".go685806154").should("not.exist");
+    cy.get(".go4109123758").should("not.exist");
+});
+
+Cypress.Commands.add("editarProductoFail", (producto, productoUpdate) => {
+    cy.buscarProducto(producto);
+    cy.get('a[id^="btn-editar-producto-"]:first').click();
+    cy.completarFormularioProducto(productoUpdate);
+    cy.get(".go685806154").should("exist");
+    cy.get(".go4109123758").should("exist");
+});
+
+Cypress.Commands.add("borrarProducto", (producto) => {
+    cy.buscarProducto(producto);
+
     cy.get('a[id^="btn-eliminar-producto-"]:first').then(($e) => {
-      if ($e.length > 0) {
-        cy.wrap($e).click();
-        cy.get(".confirm-button").click();
-      }
+        if ($e.length > 0) {
+            cy.wrap($e).click();
+            cy.get(".confirm-button").click();
+        }
     });
-  });
-  
+});
