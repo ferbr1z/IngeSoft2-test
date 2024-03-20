6dc44cd891a47ed0aa37a05eb04e6099710be5c8
Fernando Brizuela <ffba97@gmail.com>
2024-03-20 15:05:44 -0300

    Se añade el número de orden a ejecutar en el nombre del test

--- cypress/e2e/Productos/1-crear-producto.cy.js
+++ cypress/e2e/Productos/1-crear-producto.cy.js
@@ -18,7 +18,7 @@ describe("Crear producto", () => {
 
   it("Crear producto con iva 0", () => {
     const { productoIva0 } = productos;
-    cy.crearProductoSuccess(productoIva0);
+    cy.crearProducto(productoIva0);
 
     cy.get(".go685806154").should("not.exist");
     cy.get(".go4109123758").should("not.exist");
@@ -26,7 +26,7 @@ describe("Crear producto", () => {
 
   it("Crear producto con iva 5", () => {
     const { productoIva5 } = productos;
-    cy.crearProductoSuccess(productoIva5);
+    cy.crearProducto(productoIva5);
 
     cy.get(".go685806154").should("not.exist");
     cy.get(".go4109123758").should("not.exist");
@@ -34,17 +34,21 @@ describe("Crear producto", () => {
 
   it("Crear producto con iva 10", () => {
     const { productoIva10 } = productos;
-    cy.crearProductoSuccess(productoIva10);
+    cy.crearProducto(productoIva10);
 
     cy.get(".go685806154").should("not.exist");
     cy.get(".go4109123758").should("not.exist");
   });
+  
+  /************************************************
+   *************** TESTS DE ERRORES ***************
+   ************************************************/
 
   it("Crear producto con error de codigo", () => {
     cy.get(".error-container").should("not.exist");
 
     const { productoErrorCodigo } = productos;
-    cy.crearProducto(productoErrorCodigo);
+    cy.crearProductoFail(productoErrorCodigo);
 
     cy.get(".go685806154").should("exist");
   });
@@ -54,7 +58,7 @@ describe("Crear producto", () => {
 
     const { productoErrorPrecio } = productos;
 
-    cy.crearProducto(productoErrorPrecio);
+    cy.crearProductoFail(productoErrorPrecio);
 
     cy.get(".go685806154").should("exist");
   });
@@ -64,9 +68,8 @@ describe("Crear producto", () => {
 
     const { productoErrorCantidad } = productos;
 
-    cy.crearProducto(productoErrorCantidad);
+    cy.crearProductoFail(productoErrorCantidad);
 
-    cy.get(".go3958317564").should("exist");
-    cy.get(".go685806154").should("exist");
+
   });
 });
