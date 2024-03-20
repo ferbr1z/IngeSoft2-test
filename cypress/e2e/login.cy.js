e171f5fe0937321fe0a89aeb680c0002a594b599
Fernando Brizuela <ffba97@gmail.com>
2024-03-20 17:02:49 -0300

    Corregido el nombre de los comandos

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -11,7 +11,7 @@ describe("Login", () => {
 
   it("Iniciar sesión como administrador", () => {
     const { admin } = users;
-    cy.loginSuccess(admin.email, admin.password);
+    cy.login(admin.email, admin.password);
     cy.url().should("eq", CLIENTES_URL);
   });
 
@@ -22,13 +22,12 @@ describe("Login", () => {
   it("Iniciar sesión como administrador con password equivocado", () => {
     const { admin } = users;
 
-    cy.login(admin.email, "pepito").then(() => {
-      cy.get(".go4109123758").should("exist");
+    cy.loginFail(admin.email, "pepito").then(() => {
     });
   });
 
   it("Iniciar sesión con datos erroneos", () => {
-    cy.login("elcorreodepepito@mail.com", "pepito").then(() => {
+    cy.loginFail("elcorreodepepito@mail.com", "pepito").then(() => {
       cy.get(".go685806154").should("exist");
     });
   });
