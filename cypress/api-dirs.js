e28c11ec6a56230e0f35508595604a7fda200a7f
Fernando Brizuela <ffba97@gmail.com>
2024-03-22 22:17:16 -0300

    Add API_CLIENTES and API_PROVEEDORES constants

--- cypress/api-dirs.js
+++ cypress/api-dirs.js
@@ -1,3 +1,5 @@
 export const API_URL = "http://localhost:8080";
 export const API_LOGIN = `${API_URL}/auth/login`
 export const API_PRODUCTOS = `${API_URL}/productos`
+export const API_CLIENTES = `${API_URL}/clientes`
+export const API_PROVEEDORES = `${API_URL}/proveedores`
