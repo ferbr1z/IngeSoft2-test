3ff2e373193549a1f2b18f7509172cc97c85dc8b
JulioLaneri <juliobenitez@fiuni.edu.py>
2024-04-29 15:43:35 -0400

    Empleados

--- cypress/front-dirs.js
+++ cypress/front-dirs.js
@@ -1,4 +1,4 @@
-export const APP_URL = "https://powerfit.up.railway.app";
+export const APP_URL = "https://powerfit-test.up.railway.app";
 
 export const CLIENTES_URL = `${APP_URL}/clientes`;
 
@@ -11,3 +11,5 @@ export const CAJA_URL = `${APP_URL}/caja`;
 export const CAJALISTA_URL = `${APP_URL}/caja/lista`;
 
 export const FACTURAS_URL =  `${CAJA_URL}/facturas` ;
+
+export const EMPLEADOS_URL =  `${APP_URL}/users` ;
